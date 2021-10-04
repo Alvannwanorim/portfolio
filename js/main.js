fe26bf5f1503b9dfd3fb70f7bcc9e95fd4e2a842
alvan nwanorim <alvannwanorim@gmail.com>
2021-10-04 21:52:40 +0100

    added new features

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+window.addEventListener('load', () => {
+  document.querySelector('.main').classList.remove('hidden');
+  document.querySelector('.home-section').classList.add('active');
+
+  /*------------ Page Loader----------*/
+  document.querySelector('.page-loader').classList.add('fade-out');
+
+  setTimeout(() => {
+    document.querySelector('.page-loader').style.display = 'none';
+  }, 600);
+});
+
 /*--------------------Toggle Navbar--------------- */
 const navToggler = document.querySelector('.nav-toggler');
 
